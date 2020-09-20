ace7d73cb143e030e15597674b941695ff5c3e02
sujata <gunalesujata@gmail.com>
2020-09-20 17:05:29 +0530

    direct heading

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,7 +6,16 @@ export class Body extends Component {
   render() {
     return (
       <div className="container">
-        <div className="messages"></div>
+        <div className="messages">
+          <div className="top-head">
+            <div className="direct">
+              <h3>Direct</h3>
+            </div>
+            <div className="edit">
+              <i className="fas fa-edit"></i>
+            </div>
+          </div>
+        </div>
         <div className="void">
           <Message />
         </div>
