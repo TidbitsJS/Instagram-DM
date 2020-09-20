432b37db6cc0c7a8b9ed4d85d9e7c37e07eafe8c
sujata <gunalesujata@gmail.com>
2020-09-20 18:52:05 +0530

    header user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import "./header.css";
+import data from "../../Data/Data";
 
 export class Header extends Component {
   render() {
+    const creator = data[14];
     return (
       <div className="heading">
         <div className="logo">
@@ -16,7 +18,13 @@ export class Header extends Component {
           <i className="fas fa-paper-plane"></i>
           <i className="fas fa-compass"></i>
           <i className="fas fa-heart"></i>
-          <div className="avatar"></div>
+          <div className="avatar">
+            <img
+              src={creator.profile_pic}
+              alt={creator.name}
+              style={{ width: "100%", height: "100%", borderRadius: "100%" }}
+            />
+          </div>
         </div>
       </div>
     );
